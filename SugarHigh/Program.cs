5fb5dd31af7757b99c44af74612aa2b82db314b0
Ralitsa Ilieva <ralka86@abv.bg>
2020-02-20 20:58:27 +0200

    Add project files.

---
+++ SugarHigh/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SugarHigh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] firstArray = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int sugarHigh = Convert.ToInt32(Console.ReadLine());
+
+                int[] copyArray = new int[firstArray.Length];
+                firstArray.CopyTo(copyArray, 0);
+
+                Array.Sort(copyArray);
+
+                List<int> candies = new List<int>();
+                int sum = copyArray[0];
+                if (sum < sugarHigh)
+                {
+                    candies.Add(copyArray[0]);
+
+                    for (int i = 1; i < copyArray.Length; i++)
+                    {
+                        sum += copyArray[i];
+                        if (sum < sugarHigh)
+                        {
+                            candies.Add(copyArray[i]);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                List<int> indexOfCandies = new List<int>();
+                if (candies.Count == 0)
+                {
+                    Console.WriteLine("[]");
+                }
+                else
+                {
+                    foreach (var candy in candies)
+                    {
+                        indexOfCandies.Add(Array.IndexOf(firstArray, candy));
+                        indexOfCandies.Sort();
+                    }
+
+                    Console.Write("[" + string.Join(", ", indexOfCandies) + "]");
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Invalid input!!!", e);
+            }
+        }
+
+    }
+}
